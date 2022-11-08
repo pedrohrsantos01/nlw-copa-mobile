4019bc13fd94e3e9942d6d8d1dd3c4ad99214f73
Pedrohrsantos01 <pedrohrsantos01@gmail.com>
2022-11-08 20:37:03 -0300

    mport components and create Home and Find screen

--- App.tsx
+++ App.tsx
@@ -12,6 +12,9 @@ import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import { THEME } from "./src/styles/theme";
 import { SignIn } from "./src/screens/SignIn";
+import { New } from "./src/screens/New";
+import { Find } from "./src/screens/Find";
+import { Pools } from "./src/screens/Pools";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +31,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Pools /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
